712cfcabc3c6a420a97d53d988005d1641b3af73
Samhith <samhithsvb@gmail.com>
2017-02-22 01:01:37 +0530

    Just changing

--- src/com/build/one/Run.java
+++ src/com/build/one/Run.java
@@ -5,6 +5,7 @@ public class Run {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=8;
+		//SO here we are developing a supply chain management application
 
 	}
 
